e4faadab954948bceab0786f566725755982f29b
ghdgu <ghdguswl13@gmail.com>
2024-05-24 16:59:41 +0900

    feat:주석수정

--- js/dday.js
+++ js/dday.js
@@ -17,7 +17,7 @@ function find_day(){
   historyDay.innerHTML=day1+"일 "+hour1+"시간 "+min1+"분 "+sec1+"초";
 
 
-  const day2 = Math.floor(day_gap2 / (1000*60*60*24));  //디데이까지 남은 밀리세컨드초 / 하루의 밀리세컨드초 = 남은 일수
+  const day2 = Math.floor(day_gap2 / (1000*60*60*24));  
   const hour2 = Math.floor(day_gap2 / (1000*60*60) % 24);
   const min2 = Math.floor(day_gap2 / (1000*60) % 60);
   const sec2 = Math.floor(day_gap2 / 1000%60);
